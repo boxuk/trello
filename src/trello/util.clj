120ac6ac236a35692bcdbb0c550397005974fc52
Owain Lewis <owain@owainlewis.com>
2012-04-14 12:54:42 +0100

    added gravatar function

--- src/trello/util.clj
+++ src/trello/util.clj
@@ -1,5 +1,12 @@
 (ns trello.util)
 
+(defn get-gravatar-image
+  "Given a gravatar hash, return the users avatar. If it can't be found a placeholder is returned"
+  [hash]
+  (let [gravatar-hash hash
+        base-url "http://www.gravatar.com/avatar"]
+    [:img {:src (format "%s/%s?d=mm" base-url avatar-hash)}]))
+  		
 (defn filter-by-param
   "Given a result map, filter out and return only the values for
    the key specified. Utility function for inspecting collections"
