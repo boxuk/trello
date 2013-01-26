eb2e97229c2b66ec47d7265b0985dcefc1012f1e
owain lewis <owain@owainlewis.com>
2013-01-26 08:10:53 +0000

    bump clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://www.owainlewis.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]					
+  :dependencies [[org.clojure/clojure "1.4.0"]					
                  [clj-http "0.3.2"]
                  [cheshire "3.1.0"]]
   :dev-dependencies [[midje "1.3.1"]
