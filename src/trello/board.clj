c69f165ead5254fd0a4fe4b8af952e6aec0566e5
owain lewis <owain@owainlewis.com>
2013-01-27 12:27:52 +0000

    tidy up code

--- src/trello/board.clj
+++ src/trello/board.clj
@@ -4,7 +4,8 @@
   (:use [trello.client :as client]))
 
 (defn all []
-  (client/api-request :get (format "Members/my/boards/all")))
+  (client/api-request :get 
+    (format "Members/my/boards/all")))
 
 (defn get
   "Get Trello boards
@@ -14,8 +15,12 @@
       Resource options [:actions :cards :checklists :lists :membersInvited :members]
   "
   ([] (all)) ;; all
-  ([id] (client/api-request :get (format "boards/%s" id)))
-  ([id resource] (client/api-request :get (format "boards/%s/%s" id (name resource)))))
+  ([id] (client/api-request :get 
+          (format "boards/%s" id)))
+  ([id resource] 
+    (client/api-request :get 
+      (format "boards/%s/%s" id 
+        (name resource)))))
 
 (defn create
   "Create a new Trello board
@@ -27,10 +32,12 @@
      Valid Values: The id or name of the organization to add the board to.
    - idBoardSource (optional)
      Valid Values: The id of the board to copy into the new board"
-  ([name] (client/api-request :post "boards" {:name name}))
+  ([name] (client/api-request :post 
+            "boards" {:name name}))
   ([name other-params]
    (let [params (merge {:name name} other-params)]
-     (client/api-request :post "boards" {:name name}))))
+     (client/api-request :post 
+       "boards" {:name name}))))
 
 (defn close 
   "Toggle a board between closed and open
@@ -38,7 +45,8 @@
      Valid Values: true or false
   "
   [id value]
-  (client/api-request :put (format "boards/%s/closed" id) {:value value}))
+  (client/api-request :put 
+    (format "boards/%s/closed" id) {:value value}))
   
 (defn get-all-list-names-for-board
   "Return a sequence of list names for a board"
